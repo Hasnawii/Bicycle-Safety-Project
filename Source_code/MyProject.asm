eac05fbaa18230433b38197d3de6cd9731fd8144
Hasnawii <hassanhasnawi99@gmail.com>
2024-12-27 01:18:54 +0300

    Implemented turn signal input mechanism
    Works simultaneously with the flex sensor analog inputs.

--- Source_code/MyProject.asm
+++ Source_code/MyProject.asm
@@ -8,26 +8,54 @@ _interrupt:
 	MOVWF      ___savePCLATH+0
 	CLRF       PCLATH+0
 
-;MyProject.c,10 :: 		interrupt(){
-;MyProject.c,12 :: 		if(INTCON & 0x04) { // TMR0 overflow interrupt occurs every 32ms
-	BTFSS      INTCON+0, 2
+;MyProject.c,21 :: 		void interrupt() {
+;MyProject.c,23 :: 		if (INTCON & 0x01) {
+	BTFSS      INTCON+0, 0
 	GOTO       L_interrupt0
-;MyProject.c,13 :: 		tick++; //increment tick evey 32ms
+;MyProject.c,24 :: 		if (!(PORTB & 0x10)) { // Check if RB5 button is pressed
+	BTFSC      PORTB+0, 4
+	GOTO       L_interrupt1
+;MyProject.c,25 :: 		PORTB |= 0x04; // Turn on RB2
+	BSF        PORTB+0, 2
+;MyProject.c,26 :: 		rturn = 1;
+	MOVLW      1
+	MOVWF      _rturn+0
+;MyProject.c,27 :: 		}
+L_interrupt1:
+;MyProject.c,28 :: 		if (!(PORTB & 0x20)) { // Check if RB6 button is pressed
+	BTFSC      PORTB+0, 5
+	GOTO       L_interrupt2
+;MyProject.c,29 :: 		PORTB |= 0x08; // Turn on RB3
+	BSF        PORTB+0, 3
+;MyProject.c,30 :: 		lturn = 1;
+	MOVLW      1
+	MOVWF      _lturn+0
+;MyProject.c,31 :: 		}
+L_interrupt2:
+;MyProject.c,32 :: 		INTCON &= 0xFE; // Clear PORTB Change Interrupt flag
+	MOVLW      254
+	ANDWF      INTCON+0, 1
+;MyProject.c,33 :: 		}
+L_interrupt0:
+;MyProject.c,36 :: 		if (INTCON & 0x04) {
+	BTFSS      INTCON+0, 2
+	GOTO       L_interrupt3
+;MyProject.c,37 :: 		tick++; // Increment tick every 32ms
 	INCF       _tick+0, 1
-;MyProject.c,14 :: 		if(tick==2){ // this condition is true every almost 64ms
+;MyProject.c,40 :: 		if (tick == 2) {
 	MOVF       _tick+0, 0
 	XORLW      2
 	BTFSS      STATUS+0, 2
-	GOTO       L_interrupt1
-;MyProject.c,15 :: 		tick=0;
+	GOTO       L_interrupt4
+;MyProject.c,41 :: 		tick = 0;
 	CLRF       _tick+0
-;MyProject.c,16 :: 		flexA0 = ATD_read0();
+;MyProject.c,44 :: 		flexA0 = ATD_read0();
 	CALL       _ATD_read0+0
 	MOVF       R0+0, 0
 	MOVWF      _flexA0+0
 	MOVF       R0+1, 0
 	MOVWF      _flexA0+1
-;MyProject.c,17 :: 		flexD0 = (unsigned int)(flexA0*50)/1023;
+;MyProject.c,45 :: 		flexD0 = (unsigned int)(flexA0 * 50) / 1023;
 	MOVLW      50
 	MOVWF      R4+0
 	MOVLW      0
@@ -42,13 +70,13 @@ _interrupt:
 	MOVWF      _flexD0+0
 	MOVF       R0+1, 0
 	MOVWF      _flexD0+1
-;MyProject.c,18 :: 		flexA1 = ATD_read1();
+;MyProject.c,46 :: 		flexA1 = ATD_read1();
 	CALL       _ATD_read1+0
 	MOVF       R0+0, 0
 	MOVWF      _flexA1+0
 	MOVF       R0+1, 0
 	MOVWF      _flexA1+1
-;MyProject.c,19 :: 		flexD1 = (unsigned int)(flexA1*50)/1023;
+;MyProject.c,47 :: 		flexD1 = (unsigned int)(flexA1 * 50) / 1023;
 	MOVLW      50
 	MOVWF      R4+0
 	MOVLW      0
@@ -63,46 +91,121 @@ _interrupt:
 	MOVWF      _flexD1+0
 	MOVF       R0+1, 0
 	MOVWF      _flexD1+1
-;MyProject.c,20 :: 		if((flexD0>34)||(flexD1>34)){
+;MyProject.c,50 :: 		if ((flexD0 > 34) || (flexD1 > 34)) {
 	MOVF       _flexD0+1, 0
 	SUBLW      0
 	BTFSS      STATUS+0, 2
-	GOTO       L__interrupt17
+	GOTO       L__interrupt26
 	MOVF       _flexD0+0, 0
 	SUBLW      34
-L__interrupt17:
+L__interrupt26:
 	BTFSS      STATUS+0, 0
-	GOTO       L__interrupt14
+	GOTO       L__interrupt23
 	MOVF       _flexD1+1, 0
 	SUBLW      0
 	BTFSS      STATUS+0, 2
-	GOTO       L__interrupt18
+	GOTO       L__interrupt27
 	MOVF       _flexD1+0, 0
 	SUBLW      34
-L__interrupt18:
+L__interrupt27:
 	BTFSS      STATUS+0, 0
-	GOTO       L__interrupt14
-	GOTO       L_interrupt4
-L__interrupt14:
-;MyProject.c,21 :: 		PORTB = PORTB | 0x02;
+	GOTO       L__interrupt23
+	GOTO       L_interrupt7
+L__interrupt23:
+;MyProject.c,51 :: 		PORTB |= 0x02; // Turn on RB1
 	BSF        PORTB+0, 1
-;MyProject.c,22 :: 		}
-	GOTO       L_interrupt5
-L_interrupt4:
-;MyProject.c,23 :: 		else PORTB = PORTB & 0xFD;
+;MyProject.c,52 :: 		} else {
+	GOTO       L_interrupt8
+L_interrupt7:
+;MyProject.c,53 :: 		PORTB &= 0xFD; // Turn off RB1
 	MOVLW      253
 	ANDWF      PORTB+0, 1
-L_interrupt5:
-;MyProject.c,24 :: 		}
-L_interrupt1:
-;MyProject.c,25 :: 		INTCON = INTCON & 0xFB; // clear the interrupt flag
+;MyProject.c,54 :: 		}
+L_interrupt8:
+;MyProject.c,55 :: 		}
+L_interrupt4:
+;MyProject.c,58 :: 		if (rturn == 1) {
+	MOVF       _rturn+0, 0
+	XORLW      1
+	BTFSS      STATUS+0, 2
+	GOTO       L_interrupt9
+;MyProject.c,59 :: 		tick1++;
+	INCF       _tick1+0, 1
+;MyProject.c,60 :: 		ticka++;
+	INCF       _ticka+0, 1
+;MyProject.c,61 :: 		if (ticka == 15) { // Toggle RB2 every ~480ms
+	MOVF       _ticka+0, 0
+	XORLW      15
+	BTFSS      STATUS+0, 2
+	GOTO       L_interrupt10
+;MyProject.c,62 :: 		ticka = 0;
+	CLRF       _ticka+0
+;MyProject.c,63 :: 		PORTB ^= 0x04; // Toggle RB2
+	MOVLW      4
+	XORWF      PORTB+0, 1
+;MyProject.c,64 :: 		}
+L_interrupt10:
+;MyProject.c,65 :: 		if (tick1 == 150) { // Stop after ~5 seconds
+	MOVF       _tick1+0, 0
+	XORLW      150
+	BTFSS      STATUS+0, 2
+	GOTO       L_interrupt11
+;MyProject.c,66 :: 		tick1 = 0;
+	CLRF       _tick1+0
+;MyProject.c,67 :: 		rturn = 0;
+	CLRF       _rturn+0
+;MyProject.c,68 :: 		PORTB &= 0xFB; // Turn off RB2
+	MOVLW      251
+	ANDWF      PORTB+0, 1
+;MyProject.c,69 :: 		}
+L_interrupt11:
+;MyProject.c,70 :: 		}
+L_interrupt9:
+;MyProject.c,73 :: 		if (lturn == 1) {
+	MOVF       _lturn+0, 0
+	XORLW      1
+	BTFSS      STATUS+0, 2
+	GOTO       L_interrupt12
+;MyProject.c,74 :: 		tick2++;
+	INCF       _tick2+0, 1
+;MyProject.c,75 :: 		tickb++;
+	INCF       _tickb+0, 1
+;MyProject.c,76 :: 		if (tickb == 15) { // Toggle RB3 every ~480ms
+	MOVF       _tickb+0, 0
+	XORLW      15
+	BTFSS      STATUS+0, 2
+	GOTO       L_interrupt13
+;MyProject.c,77 :: 		tickb = 0;
+	CLRF       _tickb+0
+;MyProject.c,78 :: 		PORTB ^= 0x08; // Toggle RB3
+	MOVLW      8
+	XORWF      PORTB+0, 1
+;MyProject.c,79 :: 		}
+L_interrupt13:
+;MyProject.c,80 :: 		if (tick2 == 150) { // Stop after ~5 seconds
+	MOVF       _tick2+0, 0
+	XORLW      150
+	BTFSS      STATUS+0, 2
+	GOTO       L_interrupt14
+;MyProject.c,81 :: 		tick2 = 0;
+	CLRF       _tick2+0
+;MyProject.c,82 :: 		lturn = 0;
+	CLRF       _lturn+0
+;MyProject.c,83 :: 		PORTB &= 0xF7; // Turn off RB3
+	MOVLW      247
+	ANDWF      PORTB+0, 1
+;MyProject.c,84 :: 		}
+L_interrupt14:
+;MyProject.c,85 :: 		}
+L_interrupt12:
+;MyProject.c,87 :: 		INTCON &= 0xFB; // Clear Timer0 Interrupt flag
 	MOVLW      251
 	ANDWF      INTCON+0, 1
-;MyProject.c,26 :: 		}
-L_interrupt0:
-;MyProject.c,27 :: 		}
+;MyProject.c,88 :: 		}
+L_interrupt3:
+;MyProject.c,89 :: 		}
 L_end_interrupt:
-L__interrupt16:
+L__interrupt25:
 	MOVF       ___savePCLATH+0, 0
 	MOVWF      PCLATH+0
 	SWAPF      ___saveSTATUS+0, 0
@@ -114,72 +217,83 @@ L__interrupt16:
 
 _main:
 
-;MyProject.c,28 :: 		void main() {
-;MyProject.c,29 :: 		TRISB = 0x00;
-	CLRF       TRISB+0
-;MyProject.c,30 :: 		PORTB = 0x00;
+;MyProject.c,91 :: 		void main() {
+;MyProject.c,93 :: 		TRISB = 0xF1; // RB0, RB1, RB2, RB3 as outputs; RB4, RB5 as inputs
+	MOVLW      241
+	MOVWF      TRISB+0
+;MyProject.c,94 :: 		PORTB = 0x00; // Initialize PORTB to LOW
 	CLRF       PORTB+0
-;MyProject.c,31 :: 		ATD_init();
+;MyProject.c,97 :: 		ATD_init();
 	CALL       _ATD_init+0
-;MyProject.c,32 :: 		OPTION_REG = 0x07; // Osc clock/4, prescale of 256
+;MyProject.c,100 :: 		OPTION_REG = 0x07; // Osc clock/4, prescaler of 256
 	MOVLW      7
 	MOVWF      OPTION_REG+0
-;MyProject.c,33 :: 		INTCON = 0xA0; // Global Interrupt Enable and Local Enable the TMR0 Overflow Interrupt
-	MOVLW      160
+;MyProject.c,101 :: 		INTCON = 0xF8;     // Enable all interrupts
+	MOVLW      248
 	MOVWF      INTCON+0
-;MyProject.c,34 :: 		TMR0 = 0;
+;MyProject.c,102 :: 		TMR0 = 0;          // Reset Timer0
 	CLRF       TMR0+0
-;MyProject.c,35 :: 		while(1){
-L_main6:
-;MyProject.c,36 :: 		}
-	GOTO       L_main6
-;MyProject.c,37 :: 		}
+;MyProject.c,105 :: 		tick = 0;
+	CLRF       _tick+0
+;MyProject.c,106 :: 		tick1 = 0;
+	CLRF       _tick1+0
+;MyProject.c,107 :: 		tick2 = 0;
+	CLRF       _tick2+0
+;MyProject.c,108 :: 		ticka = 0;
+	CLRF       _ticka+0
+;MyProject.c,109 :: 		tickb = 0;
+	CLRF       _tickb+0
+;MyProject.c,112 :: 		while (1) {
+L_main15:
+;MyProject.c,114 :: 		}
+	GOTO       L_main15
+;MyProject.c,115 :: 		}
 L_end_main:
 	GOTO       $+0
 ; end of _main
 
 _ATD_init:
 
-;MyProject.c,39 :: 		void ATD_init(void){
-;MyProject.c,40 :: 		ADCON0 = 0x41;// ATD ON, Don't GO, Channel 0, Fosc/16
+;MyProject.c,118 :: 		void ATD_init(void) {
+;MyProject.c,119 :: 		ADCON0 = 0x41; // ADC ON, Don't GO, Channel 0, Fosc/16
 	MOVLW      65
 	MOVWF      ADCON0+0
-;MyProject.c,41 :: 		ADCON1 = 0xC0;// all analog channels, 500 KHz, right justified
+;MyProject.c,120 :: 		ADCON1 = 0xC0; // All analog channels, 500 KHz, right justified
 	MOVLW      192
 	MOVWF      ADCON1+0
-;MyProject.c,42 :: 		TRISA = 0xFF; // all PORTA is inputs
+;MyProject.c,121 :: 		TRISA = 0xFF;  // Configure PORTA as input
 	MOVLW      255
 	MOVWF      TRISA+0
-;MyProject.c,43 :: 		TRISE = 0x07; // all PORTE is inputs
+;MyProject.c,122 :: 		TRISE = 0x07;  // Configure PORTE as input
 	MOVLW      7
 	MOVWF      TRISE+0
-;MyProject.c,44 :: 		}
+;MyProject.c,123 :: 		}
 L_end_ATD_init:
 	RETURN
 ; end of _ATD_init
 
 _ATD_read0:
 
-;MyProject.c,46 :: 		unsigned int ATD_read0(void){
-;MyProject.c,47 :: 		ADCON0 = ADCON0 &  0xC7;
+;MyProject.c,126 :: 		unsigned int ATD_read0(void) {
+;MyProject.c,127 :: 		ADCON0 &= 0xC7;   // Select channel 0
 	MOVLW      199
 	ANDWF      ADCON0+0, 1
-;MyProject.c,48 :: 		delay_us(10); // Implement us delay function instead of this
+;MyProject.c,128 :: 		delay_us(10);     // Stabilize ADC input
 	MOVLW      6
 	MOVWF      R13+0
-L_ATD_read08:
+L_ATD_read017:
 	DECFSZ     R13+0, 1
-	GOTO       L_ATD_read08
+	GOTO       L_ATD_read017
 	NOP
-;MyProject.c,49 :: 		ADCON0 = ADCON0 | 0x04;// GO
+;MyProject.c,129 :: 		ADCON0 |= 0x04;   // Start ADC conversion
 	BSF        ADCON0+0, 2
-;MyProject.c,50 :: 		while(ADCON0 & 0x04);
-L_ATD_read09:
+;MyProject.c,130 :: 		while (ADCON0 & 0x04); // Wait for conversion to complete
+L_ATD_read018:
 	BTFSS      ADCON0+0, 2
-	GOTO       L_ATD_read010
-	GOTO       L_ATD_read09
-L_ATD_read010:
-;MyProject.c,51 :: 		return((ADRESH<<8) | ADRESL);
+	GOTO       L_ATD_read019
+	GOTO       L_ATD_read018
+L_ATD_read019:
+;MyProject.c,131 :: 		return ((ADRESH << 8) | ADRESL); // Return ADC result
 	MOVF       ADRESH+0, 0
 	MOVWF      R0+1
 	CLRF       R0+0
@@ -187,37 +301,37 @@ L_ATD_read010:
 	IORWF      R0+0, 1
 	MOVLW      0
 	IORWF      R0+1, 1
-;MyProject.c,52 :: 		}
+;MyProject.c,132 :: 		}
 L_end_ATD_read0:
 	RETURN
 ; end of _ATD_read0
 
 _ATD_read1:
 
-;MyProject.c,54 :: 		unsigned int ATD_read1(void){
-;MyProject.c,55 :: 		ADCON0 = (ADCON0 &  0xCF)|0x08; // select channel 1
+;MyProject.c,135 :: 		unsigned int ATD_read1(void) {
+;MyProject.c,136 :: 		ADCON0 = (ADCON0 & 0xCF) | 0x08; // Select channel 1
 	MOVLW      207
 	ANDWF      ADCON0+0, 0
 	MOVWF      R0+0
 	MOVLW      8
 	IORWF      R0+0, 0
 	MOVWF      ADCON0+0
-;MyProject.c,56 :: 		delay_us(10); // Implement us delay function instead of this
+;MyProject.c,137 :: 		delay_us(10);      // Stabilize ADC input
 	MOVLW      6
 	MOVWF      R13+0
-L_ATD_read111:
+L_ATD_read120:
 	DECFSZ     R13+0, 1
-	GOTO       L_ATD_read111
+	GOTO       L_ATD_read120
 	NOP
-;MyProject.c,57 :: 		ADCON0 = ADCON0 | 0x04;// GO
+;MyProject.c,138 :: 		ADCON0 |= 0x04;    // Start ADC conversion
 	BSF        ADCON0+0, 2
-;MyProject.c,58 :: 		while(ADCON0 & 0x04);
-L_ATD_read112:
+;MyProject.c,139 :: 		while (ADCON0 & 0x04); // Wait for conversion to complete
+L_ATD_read121:
 	BTFSS      ADCON0+0, 2
-	GOTO       L_ATD_read113
-	GOTO       L_ATD_read112
-L_ATD_read113:
-;MyProject.c,59 :: 		return((ADRESH<<8) | ADRESL);
+	GOTO       L_ATD_read122
+	GOTO       L_ATD_read121
+L_ATD_read122:
+;MyProject.c,140 :: 		return ((ADRESH << 8) | ADRESL); // Return ADC result
 	MOVF       ADRESH+0, 0
 	MOVWF      R0+1
 	CLRF       R0+0
@@ -225,7 +339,7 @@ L_ATD_read113:
 	IORWF      R0+0, 1
 	MOVLW      0
 	IORWF      R0+1, 1
-;MyProject.c,60 :: 		}
+;MyProject.c,141 :: 		}
 L_end_ATD_read1:
 	RETURN
 ; end of _ATD_read1
--- Source_code/MyProject.c
+++ Source_code/MyProject.c
@@ -1,60 +1,141 @@
+// Global Variables
 unsigned char tick;
-void ATD_init(void);
-unsigned int ATD_read0(void);
-unsigned int ATD_read1(void);
+unsigned char tick1;
+unsigned char tick2;
+unsigned char ticka;
+unsigned char tickb;
+unsigned char rturn;
+unsigned char lturn;
 unsigned int flexA0;
 unsigned int flexA1;
 unsigned int flexD0;
 unsigned int flexD1;
 
-interrupt(){
+// Function Declarations
+void ATD_init(void);
+unsigned int ATD_read0(void);
+unsigned int ATD_read1(void);
 
-if(INTCON & 0x04) { // TMR0 overflow interrupt occurs every 32ms
-   tick++; //increment tick evey 32ms
-   if(tick==2){ // this condition is true every almost 64ms
-   tick=0;
-   flexA0 = ATD_read0();
-   flexD0 = (unsigned int)(flexA0*50)/1023;
-   flexA1 = ATD_read1();
-   flexD1 = (unsigned int)(flexA1*50)/1023;
-   if((flexD0>34)||(flexD1>34)){
-   PORTB = PORTB | 0x02;   // Turn on RB1
-   }
-   else PORTB = PORTB & 0xFD;  // Turn off RB1
-   }
-INTCON = INTCON & 0xFB; // clear the interrupt flag
+
+// Interrupt Service Routine
+void interrupt() {
+    // PORTB Change Interrupt
+    if (INTCON & 0x01) {
+        if (!(PORTB & 0x10)) { // Check if RB5 button is pressed
+            PORTB |= 0x04; // Turn on RB2
+            rturn = 1;
+        }
+        if (!(PORTB & 0x20)) { // Check if RB6 button is pressed
+            PORTB |= 0x08; // Turn on RB3
+            lturn = 1;
+        }
+        INTCON &= 0xFE; // Clear PORTB Change Interrupt flag
+    }
+
+    // Timer0 Overflow Interrupt
+    if (INTCON & 0x04) {
+        tick++; // Increment tick every 32ms
+
+        // ADC Sampling Logic (~64ms)
+        if (tick == 2) {
+            tick = 0;
+
+            // Read ADC values
+            flexA0 = ATD_read0();
+            flexD0 = (unsigned int)(flexA0 * 50) / 1023;
+            flexA1 = ATD_read1();
+            flexD1 = (unsigned int)(flexA1 * 50) / 1023;
+
+            // Control RB1 based on ADC conditions
+            if ((flexD0 > 34) || (flexD1 > 34)) {
+                PORTB |= 0x02; // Turn on RB1
+            } else {
+                PORTB &= 0xFD; // Turn off RB1
+            }
+        }
+
+        // Right Turn Signal Logic
+        if (rturn == 1) {
+            tick1++;
+            ticka++;
+            if (ticka == 15) { // Toggle RB2 every ~480ms
+                ticka = 0;
+                PORTB ^= 0x04; // Toggle RB2
+            }
+            if (tick1 == 150) { // Stop after ~5 seconds
+                tick1 = 0;
+                rturn = 0;
+                PORTB &= 0xFB; // Turn off RB2
+            }
+        }
+
+        // Left Turn Signal Logic
+        if (lturn == 1) {
+            tick2++;
+            tickb++;
+            if (tickb == 15) { // Toggle RB3 every ~480ms
+                tickb = 0;
+                PORTB ^= 0x08; // Toggle RB3
+            }
+            if (tick2 == 150) { // Stop after ~5 seconds
+                tick2 = 0;
+                lturn = 0;
+                PORTB &= 0xF7; // Turn off RB3
+            }
+        }
+
+        INTCON &= 0xFB; // Clear Timer0 Interrupt flag
+    }
 }
-}
+
 void main() {
-  TRISB = 0x00;
-  PORTB = 0x00;
-  ATD_init();
-  OPTION_REG = 0x07; // Osc clock/4, prescale of 256
-  INTCON = 0xA0; // Global Interrupt Enable and Local Enable the TMR0 Overflow Interrupt
-  TMR0 = 0;
-  while(1){
-  }
- }
- 
-void ATD_init(void){
- ADCON0 = 0x41;// ATD ON, Don't GO, Channel 0, Fosc/16
- ADCON1 = 0xC0;// all analog channels, 500 KHz, right justified
- TRISA = 0xFF; // all PORTA is inputs
- TRISE = 0x07; // all PORTE is inputs
- }
- 
-unsigned int ATD_read0(void){
-  ADCON0 = ADCON0 &  0xC7;
-  delay_us(10); // Implement us delay function instead of this
-  ADCON0 = ADCON0 | 0x04;// GO
-  while(ADCON0 & 0x04);
-  return((ADRESH<<8) | ADRESL); 
-  }
-  
-unsigned int ATD_read1(void){
-  ADCON0 = (ADCON0 &  0xCF)|0x08; // select channel 1
-  delay_us(10); // Implement us delay function instead of this
-  ADCON0 = ADCON0 | 0x04;// GO
-  while(ADCON0 & 0x04);
-  return((ADRESH<<8) | ADRESL); 
-  }
+    // Configure Ports
+    TRISB = 0xF1; // RB0, RB1, RB2, RB3 as outputs; RB4, RB5 as inputs
+    PORTB = 0x00; // Initialize PORTB to LOW
+
+    // Initialize ADC
+    ATD_init();
+
+    // Configure Timer0
+    OPTION_REG = 0x07; // Osc clock/4, prescaler of 256
+    INTCON = 0xF8;     // Enable all interrupts
+    TMR0 = 0;          // Reset Timer0
+
+    // Initialize Variables
+    tick = 0;
+    tick1 = 0;
+    tick2 = 0;
+    ticka = 0;
+    tickb = 0;
+
+    // Main Loop
+    while (1) {
+        // Intentionally left empty; logic handled in ISR
+    }
+}
+
+// ADC Initialization
+void ATD_init(void) {
+    ADCON0 = 0x41; // ADC ON, Don't GO, Channel 0, Fosc/16
+    ADCON1 = 0xC0; // All analog channels, 500 KHz, right justified
+    TRISA = 0xFF;  // Configure PORTA as input
+    TRISE = 0x07;  // Configure PORTE as input
+}
+
+// Read ADC value from channel 0
+unsigned int ATD_read0(void) {
+    ADCON0 &= 0xC7;   // Select channel 0
+    delay_us(10);     // Stabilize ADC input
+    ADCON0 |= 0x04;   // Start ADC conversion
+    while (ADCON0 & 0x04); // Wait for conversion to complete
+    return ((ADRESH << 8) | ADRESL); // Return ADC result
+}
+
+// Read ADC value from channel 1
+unsigned int ATD_read1(void) {
+    ADCON0 = (ADCON0 & 0xCF) | 0x08; // Select channel 1
+    delay_us(10);      // Stabilize ADC input
+    ADCON0 |= 0x04;    // Start ADC conversion
+    while (ADCON0 & 0x04); // Wait for conversion to complete
+    return ((ADRESH << 8) | ADRESL); // Return ADC result
+}
